9918ca72ce21a0a0addde5bed28490cd7dc093da
Andy Jhonson <andy645jh@gmail.com>
2018-08-01 22:53:42 -0500

    Se agrego movimiento ondulario del raycast

--- Assets/PlayerRayTest.cs
+++ Assets/PlayerRayTest.cs
@@ -11,13 +11,35 @@ public class PlayerRayTest : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Forward: "+transform.forward);
+		angle =0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var posX = Mathf.Tan(Mathf.Deg2Rad*(angle+target.transform.eulerAngles.y));
-		Debug.DrawRay(target.position, transform.forward * 5, Color.magenta);
-		Debug.DrawRay(target.position, new Vector3(posX,0,1) * 5, Color.green);
+		var finalAngle =angle+target.transform.eulerAngles.y;
+		var posX = Mathf.Tan(Mathf.Deg2Rad*(finalAngle));
+		//Debug.Log("Angle: "+finalAngle+" -- PosX: "+posX);
+		/*if(finalAngle<270){
+			posX*=-1;
+		}*/
+
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			angle += 1f;
+		}
+
+		if(Input.GetKeyDown(KeyCode.DownArrow)){
+			angle -= 1f;
+		}
+		
+		var x = transform.forward.x*Mathf.Cos(Mathf.Deg2Rad*angle) - transform.forward.z*Mathf.Sin(Mathf.Deg2Rad*angle);
+		var z = transform.forward.x*Mathf.Sin(Mathf.Deg2Rad*angle) + transform.forward.z*Mathf.Cos(Mathf.Deg2Rad*angle);
+		var w = transform.forward.x*Mathf.Cos(Mathf.Deg2Rad*-angle) - transform.forward.z*Mathf.Sin(Mathf.Deg2Rad*-angle);
+		var y = transform.forward.x*Mathf.Sin(Mathf.Deg2Rad*-angle) + transform.forward.z*Mathf.Cos(Mathf.Deg2Rad*-angle);
+		Debug.Log("Forward "+transform.forward);
+		Debug.DrawRay(target.position, new Vector3(x,0,z) * 5, Color.magenta);
+		//Debug.DrawRay(target.position, transform.forward * 5, Color.green);
+		//Debug.DrawRay(target.position, new Vector3(w,0,y) * 5, Color.cyan);
+		//Debug.DrawRay(target.position, new Vector3(posX,0,1) * 5, Color.green);
 		/*
 		Debug.DrawRay(target.position, target.forward * 5, Color.yellow);
 		
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -12,6 +12,7 @@ public class EnemyAI : MonoBehaviour {
 	public float distanceFront = 30;
 	public float distanceBack = 10;
 	public float followTime= 5;
+	public float maxAngle= 15;
 #endregion
 
 #region private properties
@@ -29,6 +30,8 @@ public class EnemyAI : MonoBehaviour {
 	private Rigidbody _playerRigi;
 	private float _inc = 0.1f;
 	private float _factor = 1;
+	public float _angle = 0;
+
 #endregion
 
 	void Start () {
@@ -36,64 +39,36 @@ public class EnemyAI : MonoBehaviour {
 		_agent = GetComponent<NavMeshAgent>();			
 		_agent.SetDestination(_positions[_counter].position);		
 		_playerRigi = player.GetComponent<Rigidbody>();		
+		//_agent.Stop();
 	}
-	
-	IEnumerator RotateAgent(Quaternion currentRotation, Quaternion targetRotation) {
-		_isRotating = true;
-		while(currentRotation != targetRotation) {
-			transform.rotation = Quaternion.RotateTowards(currentRotation, targetRotation, _rotationSpeed * Time.deltaTime);
-			yield return 1;
-		}
-		_isRotating = false;
-	}
-
-	private void rotate(){
-
-		if(_isRotating){
-			Vector3 targetDir = _pos - transform.position;
-			// The step size is equal to speed times frame time.
-			float step = _speed * Time.deltaTime;
-			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);	
-			
-			Debug.DrawRay(transform.position, newDir, Color.red);
-
-			// Move our position a step closer to the target.
-			transform.rotation = Quaternion.LookRotation(newDir);		
-			Debug.Log("Target: "+targetDir);	
-			Debug.Log("Dir: "+newDir);
-			if(targetDir==newDir || _isFollowPlayer){
-				_isRotating = false;
-			}
-		}		
-	}
-
 	private void moverPlayer(){
 		float v = Input.GetAxisRaw("Vertical");
-		float h = Input.GetAxisRaw("Horizontal");	
-	
-		_playerRigi.velocity = new Vector3(10 * h, 0, 10 * v);
-		Debug.Log("Vel: "+_playerRigi.velocity);
+		float h = Input.GetAxisRaw("Horizontal");		
+		_playerRigi.velocity = new Vector3(10 * h, 0, 10 * v);		
 	}
 
 	// Update is called once per frame
+	
 	void Update () {
-
-		/*rayTarget.eulerAngles = Vector3.up * _inc;
-		_inc += (0.8f * _factor);
-		Debug.Log("Incremento: "+_inc);
-		if(_inc>15){
+		
+		var x = transform.forward.x*Mathf.Cos(Mathf.Deg2Rad*_angle) - transform.forward.z*Mathf.Sin(Mathf.Deg2Rad*_angle);
+		var z = transform.forward.x*Mathf.Sin(Mathf.Deg2Rad*_angle) + transform.forward.z*Mathf.Cos(Mathf.Deg2Rad*_angle);
+		_angle += (0.8f * _factor);
+		//Debug.Log("Incremento: "+_angle);
+		if(_angle>maxAngle){
 			_factor = -1;
-		}else if(_inc<-15){
+		}else if(_angle<-maxAngle){
 			_factor = 1;
-		}*/
+		}
 
-		moverPlayer();		
-		rotate();
+		moverPlayer();
 
 		float dist=_agent.remainingDistance;
 		
-		bool detectPlayerFront = Physics.Raycast(rayTarget.position, transform.forward, out _hitFront, distanceFront);
+		//bool detectPlayerFront = Physics.Raycast(rayTarget.position, transform.forward, out _hitFront, distanceFront);
+		bool detectPlayerFront = Physics.Raycast(rayTarget.position, new Vector3(x,0,z), out _hitFront, distanceFront);
 		bool detectPlayerBack = Physics.Raycast(rayTarget.position, -transform.forward, out _hitBack, distanceBack);
+		
 
 		if(_isWaiting){
 			_agent.SetDestination(player.position);		
@@ -113,24 +88,21 @@ public class EnemyAI : MonoBehaviour {
 			}
 			              
 			Debug.Log("Se encontro al Player");
-			Debug.DrawRay(rayTarget.position, transform.forward * Vector3.Distance(_hitFront.point,transform.position), Color.green);		
+			Debug.DrawRay(rayTarget.position, new Vector3(x,0,z) * Vector3.Distance(_hitFront.point,transform.position), Color.green);
+			//Debug.DrawRay(rayTarget.position, transform.forward * Vector3.Distance(_hitFront.point,transform.position), Color.green);		
 			
 		}else{				
 				
-			Debug.Log("esta colisionando con Muros");
+			//Debug.Log("esta colisionando con Muros");
 			//_agent.speed = 3.5f; 
 			if(_isFollowPlayer){				
 				//_agent.SetDestination(_positions[_counter].position);
 				_isFollowPlayer = false;
 			}
-			
-			var posX = Mathf.Tan(Mathf.Deg2Rad*(10+transform.eulerAngles.y));
-			var posY = Mathf.Tan(Mathf.Deg2Rad*(-10+transform.eulerAngles.y));
-			Debug.DrawRay(rayTarget.position, new Vector3(posX,0,1) * Vector3.Distance(_hitFront.point,transform.position), Color.magenta);
-			Debug.DrawRay(rayTarget.position, new Vector3(posY,0,1) * Vector3.Distance(_hitFront.point,transform.position), Color.cyan);
-
-			Debug.DrawRay(rayTarget.position, transform.forward * Vector3.Distance(_hitFront.point,transform.position), Color.yellow);			
-			Debug.DrawRay(rayTarget.position, -transform.forward * Vector3.Distance(_hitBack.point,transform.position), Color.yellow);			
+						
+			Debug.DrawRay(rayTarget.position, new Vector3(x,0,z) * Vector3.Distance(_hitFront.point,transform.position), Color.magenta);			
+			//Debug.DrawRay(rayTarget.position, transform.forward * Vector3.Distance(_hitFront.point,transform.position), Color.yellow);			
+			//Debug.DrawRay(rayTarget.position, -transform.forward * Vector3.Distance(_hitBack.point,transform.position), Color.yellow);			
         }
 
 		if (dist!=Mathf.Infinity && _agent.pathStatus==NavMeshPathStatus.PathComplete && _agent.remainingDistance==0) {			

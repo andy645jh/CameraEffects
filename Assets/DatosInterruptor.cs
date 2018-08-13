0642911d111f39e7aa6b655fb67cbc1044ba8a36
Andy Jhonson <andy645jh@gmail.com>
2018-08-12 20:13:05 -0500

    Se agregan archivos q podrian ser necesarios

---
+++ Assets/DatosInterruptor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosInterruptor : MonoBehaviour {
+
+    public GameObject[] luces;
+    public bool Encendidas;
+    UIManager uimanager;
+
+    // Use this for initialization
+    void Start () {
+        uimanager = GameObject.Find("UI Manager").transform.GetComponent<UIManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toggleLuces ()
+    {
+        if (Encendidas)
+
+        {
+            //if (uimanager.doorButton.activeSelf == false)
+            //{
+            //    uimanager.doorButton.SetActive(true);
+            //}
+
+            for (int i = 0; i < luces.Length; i++)
+            {
+                luces[i].GetComponent<Light>().enabled = false;
+                Encendidas = false;
+                print("Apagas la luz");
+            }
+
+        }
+        else
+        {
+            //if (uimanager.doorButton.activeSelf == false)
+            //{
+            //    uimanager.doorButton.SetActive(true);
+            //}
+            for (int i = 0; i < luces.Length; i++)
+            {
+                luces[i].GetComponent<Light>().enabled = true;
+                Encendidas = true;
+                print("Enciendes");
+            }
+        }
+    }
+}
